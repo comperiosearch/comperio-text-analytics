f71ed6340d06c6bef7142c252ddbd338a7a9c61a
Christoffer Vig <christoffer.vig@comperiosearch.com>
2015-10-23 14:45:00 +0200

    Added parameters for data-dir - where to save the models, vocab - to supply a pre built vocabulary - w2v-size and w2v-window for word2vec configuration
    Set up logging to file with timestamp

---
+++ python-client/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='es_text_analytics',
+    version='0.1',
+    packages=['es_text_analytics', 'es_text_analytics.test'],
+    url='https://bitbucket.org/comperio/comperio-text-analytics',
+    license='For internal use only.',
+    author='Andre Lynum',
+    author_email='andre.lynum@comperiosearch.com',
+    description='es text analytics.',
+    install_requires=['elasticsearch', 'requests', 'psutil', 'codecs', 'textblob', 'nltk', 'abc','gensim', 'uniseg'],
+
+)
