fe43d9da12e64663540f99a2faa2b078a8ddb223
André Lynum <and-ly@online.no>
2016-01-23 20:23:22 +0100

    Code cleanup.

--- python-client/sklext/mutual_information.py
+++ python-client/sklext/mutual_information.py
@@ -2,17 +2,18 @@ from itertools import izip
 from math import log
 
 import numpy
-from numpy import array, sum, zeros, newaxis
+from numpy import array, sum, zeros
 from scipy.sparse import issparse
 
 
-def add_smoothing(m, amount=10**-12):
+def add_smoothing(m, amount=10 ** -12):
     m = m.astype(numpy.float)
     m[m == 0] = amount
 
     return m
 
-def indicator_estimator(X, axis=0):
+
+def marginal_estimator(X, axis=0):
     N = X.shape[axis]
 
     if issparse(X):
@@ -20,38 +21,9 @@ def indicator_estimator(X, axis=0):
     else:
         p = array(sum(X > 0, axis=axis)).flatten() / float(N)
 
-    p[p == 0] = 10**-12
+    p[p == 0] = 10 ** -12
 
     return p
-
-
-def joint_estimator(X, y):
-    num_terms = X.shape[1]
-    num_classes = y.shape[1]
-
-    if issparse(y):
-        N_c = y.sum(axis=0)
-    else:
-        N_c = sum(y, axis=0)
-
-    p_t_c = 1 * (X > 0).T.dot(y)
-
-    if issparse(p_t_c):
-        p_t_c = p_t_c.todense()
-
-    p_nt_c = zeros((num_terms, num_classes))
-
-    for i in xrange(num_terms):
-        p_nt_c[i,:] = N_c - p_t_c[i,:]
-
-    tot = array(sum(p_t_c + p_nt_c, axis=1)).flatten()
-    p_t_c = p_t_c / tot[:,newaxis]
-    p_nt_c = p_nt_c / tot[:,newaxis]
-
-    p_t_c[p_t_c == 0] = 10**-12
-    p_nt_c[p_nt_c == 0] = 10**-12
-
-    return p_t_c, p_nt_c
 
 
 def joint_estimator_point(X, y, smoothing=False):
@@ -63,7 +35,7 @@ def joint_estimator_point(X, y, smoothing=False):
     if smoothing:
         counts = add_smoothing(counts)
 
-    return counts/numpy.sum(counts, dtype=numpy.float)
+    return counts / numpy.sum(counts, dtype=numpy.float)
 
 
 def joint_estimator_full_sparse(X, y, smoothing=False):
@@ -81,39 +53,39 @@ def joint_estimator_full_sparse(X, y, smoothing=False):
         c_mask = zeros(c, dtype=numpy.bool)
         c_mask[c_idx] = True
 
-        counts[0][t_mask,c_mask] += 1
-        counts[1][t_mask,~c_mask] += 1
-        counts[2][~t_mask,c_mask] += 1
-        counts[3][~t_mask,~c_mask] += 1
+        counts[0][t_mask, c_mask] += 1
+        counts[1][t_mask, ~c_mask] += 1
+        counts[2][~t_mask, c_mask] += 1
+        counts[3][~t_mask, ~c_mask] += 1
 
     if smoothing:
         counts = [add_smoothing(m) for m in counts]
 
     total = numpy.sum([numpy.sum(m) for m in counts], dtype=numpy.float)
 
-    return [m/total for m in counts]
+    return [m / total for m in counts]
 
 
 def joint_estimator_full(X, y, smoothing=False):
     if issparse(X) or issparse(y):
         return joint_estimator_full_sparse(X, y, smoothing=smoothing)
 
-    counts = [xx.T.dot(yy) for xx, yy in zip([X, X, 1-X, 1-X], [y, 1-y, y, 1-y])]
+    counts = [xx.T.dot(yy) for xx, yy in zip([X, X, 1 - X, 1 - X], [y, 1 - y, y, 1 - y])]
 
     if smoothing:
         counts = [add_smoothing(m) for m in counts]
 
     total = numpy.sum([numpy.sum(m) for m in counts], dtype=numpy.float)
 
-    return [m/total for m in counts]
+    return [m / total for m in counts]
 
 
 def mutual_information(X, y):
     num_terms = X.shape[1]
     num_classes = y.shape[1]
 
-    p_c = indicator_estimator(y)
-    p_t = indicator_estimator(X)
+    p_c = marginal_estimator(y)
+    p_t = marginal_estimator(X)
 
     p_t_c = joint_estimator_full(X, y, smoothing=True)
 
@@ -121,17 +93,17 @@ def mutual_information(X, y):
 
     for i in xrange(num_terms):
         for j in xrange(num_classes):
-            ig[i] += p_t_c[0][i, j] * log(p_t_c[0][i, j]/(p_t[i]*p_c[j]))
-            ig[i] += p_t_c[1][i, j] * log(p_t_c[1][i, j]/(p_t[i]*(1- p_c[j])))
-            ig[i] += p_t_c[2][i, j] * log(p_t_c[2][i, j]/((1 - p_t[i])*p_c[j]))
-            ig[i] += p_t_c[3][i, j] * log(p_t_c[3][i, j]/((1 - p_t[i])*(1- p_c[j])))
+            ig[i] += p_t_c[0][i, j] * log(p_t_c[0][i, j] / (p_t[i] * p_c[j]))
+            ig[i] += p_t_c[1][i, j] * log(p_t_c[1][i, j] / (p_t[i] * (1 - p_c[j])))
+            ig[i] += p_t_c[2][i, j] * log(p_t_c[2][i, j] / ((1 - p_t[i]) * p_c[j]))
+            ig[i] += p_t_c[3][i, j] * log(p_t_c[3][i, j] / ((1 - p_t[i]) * (1 - p_c[j])))
 
     return ig
 
 
 def pointwise_mutual_information(X, y, normalize=False):
-    p_c = indicator_estimator(y)
-    p_t = indicator_estimator(X)
+    p_c = marginal_estimator(y)
+    p_t = marginal_estimator(X)
 
     p_t.shape = 2, 1
     p_c.shape = 1, 2
